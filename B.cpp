be42fd329a01d740b34c201cc40a0934e1cc70ad
sazzad188 <sazzad213456789@gmail.com>
2025-05-03 10:27:32 +0600

    Create B.cpp

---
+++ B.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a;
+    cin>>a;
+    long long b;
+    cin>>b;
+    char c;
+    cin>>c;
+    float d;
+    cin>>d;
+    double e;
+    cin>>e;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    return 0;
+}
